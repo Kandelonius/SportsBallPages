e32ed2763eb8479b24b847ad4685ec630af358f1
Shane Kaestner <shane.kaestner@yahoo.com>
2022-08-16 22:53:44 -0700

    minor updates

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>It&apos;s sportsball time!!</title>
         <meta name="description" content="It's sportsball time!!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
